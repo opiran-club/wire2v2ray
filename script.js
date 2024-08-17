5d474a2d565170b149e2a4c1293e8295fa116b49
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-08-17 07:16:31 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function convertToURL() {
+functionconvertToURL() {
     const name = document.getElementById('name').value;
     const address = document.getElementById('address').value;
     const port = document.getElementById('port').value;
@@ -8,11 +8,13 @@ function convertToURL() {
     const peerSecretKey = document.getElementById('peerSecretKey').value;
     const peerPublicKey = document.getElementById('peerPublicKey').value;
     const allowedIPs = document.getElementById('allowedIPs').value;
-    const url = `wireguard://${encodeURIComponent(peerSecretKey)}@${address}:${port}/?publickey=${encodeURIComponent(peerPublicKey)}&address=${encodeURIComponent(allowedIPs)}&mtu=${encodeURIComponent(mtu)#${encodeURIComponent(name)}`;
+
+    const url = `wireguard://${encodeURIComponent(peerSecretKey)}@${address}:${port}/?publickey=${encodeURIComponent(peerPublicKey)}&address=${encodeURIComponent(allowedIPs)}&mtu=${encodeURIComponent(mtu)}#${encodeURIComponent(name)}`;
+    
     document.getElementById('wg-uri').innerText = url;
 }
 
-function copyToClipboard() {
+functioncopyToClipboard() {
     const urlElement = document.getElementById('wg-uri');
     const urlText = urlElement.innerText;
     navigator.clipboard.writeText(urlText).then(() => {
